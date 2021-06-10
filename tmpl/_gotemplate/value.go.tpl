cc3c6f0c1537dcf5d8c707ddb7711d106969a3ee
steve high <steve@thentwrk.com>
2021-06-10 10:26:19 -0400

    fixed template

--- tmpl/_gotemplate/value.go.tpl
+++ tmpl/_gotemplate/value.go.tpl
@@ -1,7 +1,7 @@
 // Value returns a copy of the underlying map[{{.KeyType}}]{{.ValueType}}
 func ({{.ReceiverName}} *{{.StructName}}) Value() map[{{.KeyType}}]{{.ValueType}} {
-	{{.ReceiverName}}.mx.RLock()
 	defer {{.ReceiverName}}.mx.RUnlock()
+{{.ReceiverName}}.mx.RLock()
 
 	out := make(map[{{.KeyType}}]{{.ValueType}}, len({{.ReceiverName}}.impl))
 	for k, v := range {{.ReceiverName}}.impl {
--- tmpl/tmpl.go
+++ tmpl/tmpl.go
@@ -164,8 +164,8 @@ defer {{.ReceiverName}}.mx.Unlock()
 // ValueTmpl wraps the `Value` func
 const ValueTmpl = `// Value returns a copy of the underlying map[{{.KeyType}}]{{.ValueType}}
 func ({{.ReceiverName}} *{{.StructName}}) Value() map[{{.KeyType}}]{{.ValueType}} {
-	{{.ReceiverName}}.mx.RLock()
 	defer {{.ReceiverName}}.mx.RUnlock()
+{{.ReceiverName}}.mx.RLock()
 
 	out := make(map[{{.KeyType}}]{{.ValueType}}, len({{.ReceiverName}}.impl))
 	for k, v := range {{.ReceiverName}}.impl {
