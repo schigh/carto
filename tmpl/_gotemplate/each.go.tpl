569904c43349e30541299d9d182ee0b9adf1a4e6
steve high <steve@thentwrk.com>
2021-06-16 14:23:44 -0400

    changed lock type for Each to write locker

--- tmpl/_gotemplate/each.go.tpl
+++ tmpl/_gotemplate/each.go.tpl
@@ -3,10 +3,11 @@
 // map will halt.
 // This function does not mutate the underlying map, although the values
 // of the map may be mutated in place
-// !!!Warning: calls to {{.ReceiverName}}.Set or {{.ReceiverName}}.Delete will deadlock !!!
+// 		!!! Warning: calls to any mutating functions of {{.StructName}}
+//		!!! will deadlock if called from within the supplied function
 func ({{.ReceiverName}} *{{.StructName}}) Each(f func(key {{.KeyType}}, value {{.ValueType}}) bool) {
-	defer {{.ReceiverName}}.mx.RUnlock()
-	{{.ReceiverName}}.mx.RLock()
+	defer {{.ReceiverName}}.mx.Unlock()
+	{{.ReceiverName}}.mx.Lock()
 
 	for _k := range {{.ReceiverName}}.impl {
 		_v := {{.ReceiverName}}.impl[_k]
--- tmpl/tmpl.go
+++ tmpl/tmpl.go
@@ -9,17 +9,17 @@ The templates are maintained there for greater readability.
 
 // MapTmpl contains all the necessary data to generate a carto map
 type MapTmpl struct {
-	GenDate          string
-	PackageName      string
-	StructName       string
-	KeyTypePackage   string
-	KeyType          string
-	ValueType        string
-	ValueTypePackage string
-	ReceiverName     string
-	GetReturnsBool   bool
-	LazyInstantiates bool
-	GetDefault       bool
+	GenDate               string
+	PackageName           string
+	StructName            string
+	KeyTypePackage        string
+	KeyType               string
+	ValueType             string
+	ValueTypePackage      string
+	ReceiverName          string
+	GetReturnsBool        bool
+	LazyInstantiates      bool
+	GetDefault            bool
 }
 
 // HeadTmpl is the file header, including imports and struct declaration.
@@ -192,10 +192,11 @@ const EachTmpl = `// Each runs a function over each key/value pair in the {{.Str
 // map will halt.
 // This function does not mutate the underlying map, although the values
 // of the map may be mutated in place
-// !!!Warning: calls to {{.ReceiverName}}.Set or {{.ReceiverName}}.Delete will deadlock !!!
+// 		!!! Warning: calls to any mutating functions of {{.StructName}}
+//		!!! will deadlock if called from within the supplied function
 func ({{.ReceiverName}} *{{.StructName}}) Each(f func(key {{.KeyType}}, value {{.ValueType}}) bool) {
-	defer {{.ReceiverName}}.mx.RUnlock()
-	{{.ReceiverName}}.mx.RLock()
+	defer {{.ReceiverName}}.mx.Unlock()
+	{{.ReceiverName}}.mx.Lock()
 
 	for _k := range {{.ReceiverName}}.impl {
 		_v := {{.ReceiverName}}.impl[_k]
